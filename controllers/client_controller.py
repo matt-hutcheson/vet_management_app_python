2748c1162da37210531ea8b0055f3b462c074f9c
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-10-03 22:14:33 +0100

    added vet_id to client class and added index route to client controller

--- controllers/client_controller.py
+++ controllers/client_controller.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template, request, rediret
+from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 
 from models.src.client import Client
+from models.src.vet import Vet
+from models.src.patient import Patient
 
 import repositories.client_repository as client_repository
+import repositories.vet_repository as vet_repository
+import repositories.patient_repository as patient_repository
 
 clients_blueprint = Blueprint("clients", __name__)
 
 # INDEX
 @clients_blueprint.route("/clients")
 def clients():
+    vets = []
+    pets = []
     clients = client_repository.select_all()
+    for client in clients:
+        vet = vet_repository.select(client.vet_id)
+        vets.append(vet)
+        # add code for patients of client once written
+    return render_template('/clients/index.html', all_clients=clients, vets=vets, pets=pets)
--- models/src/client.py
+++ models/src/client.py
@@ -1,8 +1,9 @@
 class Client():
-    def __init__(self, first_name, last_name, phone_number, address, registered=False, id=None):
+    def __init__(self, first_name, last_name, phone_number, address, registered=False, vet_id=None, id=None):
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
         self.address = address
         self.registered = registered
         self.id = id
+        self.vet_id = vet_id
