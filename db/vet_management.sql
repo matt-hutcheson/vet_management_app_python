402189ff71ce874388a098978256d20d17dcd481
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-10-03 12:44:10 +0100

    added client table to sql

--- db/vet_management.sql
+++ db/vet_management.sql
@@ -1,8 +1,18 @@
 DROP TABLE if exists vets;
+DROP TABLE if exists clients;
 
 CREATE TABLE vets (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     job_title VARCHAR(255)
+)
+
+CREATE TABLE clients (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    phone_number VARCHAR(255),
+    address VARCHAR(MAX),
+    registered BOOLEAN
 )
