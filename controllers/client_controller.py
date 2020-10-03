e99cb20d1f0b0e142390ac965ce50dc924d1ecd6
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-10-03 21:51:52 +0100

    added select_all to client repo and index to client controller

--- controllers/client_controller.py
+++ controllers/client_controller.py
@@ -1,1 +1,13 @@
 from flask import Flask, render_template, request, rediret
+from flask import Blueprint
+
+from models.src.client import Client
+
+import repositories.client_repository as client_repository
+
+clients_blueprint = Blueprint("clients", __name__)
+
+# INDEX
+@clients_blueprint.route("/clients")
+def clients():
+    clients = client_repository.select_all()
--- repositories/client_repository.py
+++ repositories/client_repository.py
@@ -8,3 +8,14 @@ def save(client):
     results = run_sql(sql, values)
     client.id = results[0]['id']
     return client
+
+def select_all():
+    clients = []
+    sql = "SELECT * FROM clients"
+    results = run_sql(sql)
+    for row in results:
+        client = Client(row['first_name'],row['last_name'], row['phone_number'], row['address'], row['registered'], row['id'])
+        clients.append(client)
+    return clients
+
+    
