bb4ff646ac47e2c3f50feab7d761feb6185e2175
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-10-03 12:04:17 +0100

    added test_patient_exists

---
+++ models/tests/test_patient.py
@@ -0,0 +1,26 @@
+import unittest
+
+from models.src.patient import Patient
+
+class TestPatient(unittest.TestCase):
+    def setUp(self):
+        self.test_patient = Patient("Rex", "05/05/2019", "Dog", "Bulldog", "Male", "Alive", "02/11/2020", "05/11/2020")
+
+    def test_patient_exists(self):
+        self.assertEqual("Rex", self.test_patient.first_name)
+        self.assertEqual("05/05/2019", self.test_patient.dob)
+        self.assertEqual("Dog", self.test_patient.type)
+        self.assertEqual("Bulldog", self.test_patient.breed)
+        self.assertEqual("Male", self.test_patient.gender)
+        self.assertEqual("Alive", self.test_patient.status)
+        self.assertEqual("02/11/2020", self.test_patient.check_in_date)
+        self.assertEqual("05/11/2020", self.test_patient.check_out_date)
+        self.assertEqual(None, self.test_patient.id)
+        self.assertEqual(None, self.test_patient.client_id)
+        self.assertEqual(None, self.test_patient.vet_id)
+
+    def test_age_to_date(self):
+        pass
+
+    def test_date_to_age(self):
+        pass
