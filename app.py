c0975d9b3c643328c3e16f247739650c17b01209
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-10-03 16:20:49 +0100

    added vet controller imports and index. Added vets/index.html and checked that it worked through flask.

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template
 
+from controllers.vet_controller import vets_blueprint
 # from controllers.client_controller import clients_blueprint
 # from controllers.patient_controller import patients_blueprint
 # from controllers.treatment_controller import treatments_blueprint
-# from controllers.vet_controller import vets_blueprint
 
 app = Flask(__name__)
 
 # app.register_blueprint(clients_blueprint)
 # app.register_blueprint(patients_blueprint)
 # app.register_blueprint(treatments_blueprint)
-# app.register_blueprint(vets_blueprint)
+app.register_blueprint(vets_blueprint)
 
 @app.route('/')
 def home():
--- controllers/vet_controller.py
+++ controllers/vet_controller.py
@@ -1,6 +1,14 @@
-# from flask import Flask, render_template, request, redirect
-# from flask import Blueprint
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
 
-# from models.src.vet import Vet
+from models.src.vet import Vet
 
-# import repositories.
+import repositories.vet_repository as vet_repository
+
+vets_blueprint = Blueprint("vets", __name__)
+
+# INDEX
+@vets_blueprint.route("/vets")
+def vets():
+    vets = vet_repository.select_all()
+    return render_template("vets/index.html", all_vets = vets)
