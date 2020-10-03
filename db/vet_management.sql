fa1f7920908b7e06dc1ab7e68b543490a5e77edb
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-10-03 12:50:40 +0100

    added patients table to sql

--- db/vet_management.sql
+++ db/vet_management.sql
@@ -1,18 +1,34 @@
+DROP TABLE if exists patients;
 DROP TABLE if exists vets;
 DROP TABLE if exists clients;
+
 
 CREATE TABLE vets (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     job_title VARCHAR(255)
-)
+);
 
 CREATE TABLE clients (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     phone_number VARCHAR(255),
-    address VARCHAR(MAX),
+    address TEXT,
     registered BOOLEAN
-)
+);
+
+CREATE TABLE patients (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    dob VARCHAR(255),
+    type VARCHAR(255),
+    breed VARCHAR(255),
+    gender BOOLEAN,
+    status VARCHAR(255),
+    check_in_date VARCHAR(255),
+    check_out_date VARCHAR(255),
+    client_id INT REFERENCES clients(id) ON DELETE CASCADE,
+    vet_id INT REFERENCES vets(id) ON DELETE CASCADE
+);
