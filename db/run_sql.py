11d1f37dcdd6d8c22c6e50c13b978d84f4bd0545
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2021-01-20 09:45:50 +0000

    heroke db prep complete

--- db/run_sql.py
+++ db/run_sql.py
@@ -1,12 +1,15 @@
+import os
 import psycopg2
 import psycopg2.extras as ext
-    
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
 def run_sql(sql, values = None):
     conn = None
     results = []
 
     try:
-        conn = psycopg2.connect("dbname='vet_management'")
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
         cur = conn.cursor(cursor_factory=ext.DictCursor)
         cur.execute(sql, values)
         conn.commit()
